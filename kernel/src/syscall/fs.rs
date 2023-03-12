808e3c91966f83e2eae1011b4b86d3b0dadcde7b
xxjay <760409558@qq.com>
2023-03-12 11:00:08 +0800

    fix: 修复代码错误

--- kernel/src/syscall/fs.rs
+++ kernel/src/syscall/fs.rs
@@ -6,7 +6,7 @@ const FD_STDOUT: usize = 1;
 pub fn sys_write(fd: usize, buf_ptr: usize, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
-            let buffers = current_proc_translate_buffer();
+            let buffers = current_proc_translate_buffer(buf_ptr, len);
             for buf in buffers {
                 print_buf(buf);
             }
