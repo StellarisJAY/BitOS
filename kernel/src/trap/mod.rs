3bfba07c04e58904c096efb316dfa3879ca6c854
xxjay <760409558@qq.com>
2023-03-13 15:31:55 +0800

    feat: 内核陷入处理器

--- kernel/src/trap/mod.rs
+++ kernel/src/trap/mod.rs
@@ -1,33 +1,55 @@
 use riscv::register::scause::{self, Trap::{Interrupt, Exception}};
-use riscv::register::stvec;
+use riscv::register::{stvec, sepc};
+use riscv::register::scause::Interrupt::*;
+use riscv::register::scause::Exception::*;
 use context::TrapContext;
 
 pub mod context;
 
+extern "C" {
+    fn _user_vec();
+    fn _kernel_vec();
+}
+
 pub fn trap_init() {
     unsafe {
         // stvec寄存器设置为trap_handler
-        stvec::write(trap_handler as usize, stvec::TrapMode::Direct);
-    }
-}
-
-#[no_mangle]
-pub fn trap_handler() {
-    let cause = scause::read();
-    // 临时trap_handler
-    match cause.cause() {
-        Interrupt(e) => {
-            println!("interrupt, code: {}", cause.code());
-        },
-        Exception(e) => {
-            panic!("exception, code: {}", cause.code());
-        },
+        stvec::write(_kernel_vec as usize, stvec::TrapMode::Direct);
     }
 }
 
 #[no_mangle]
 pub fn user_trap_handler() {
-    
+    let scause = scause::read();
+    match scause.cause() {
+        // 用户进程ecall导致的系统调用
+        Exception(UserEnvCall) => {
+
+        },
+        // 由machine模式时间中断处理器抛出的S模式软件中断
+        Interrupt(SupervisorSoft) => {
+
+        },
+        _ => panic!("unhandled trap"),
+    }
+}
+
+#[no_mangle]
+pub fn kernel_trap_handler() {
+    let scause = scause::read();
+    let mut epc = sepc::read();
+    epc += 4;
+    sepc::write(epc);
+    match scause.cause() {
+        // 由machine模式时间中断处理器抛出的S模式软件中断
+        Interrupt(e) => {
+            debug!("interrupt: {:?}", e);
+        },
+        Exception(e) => {
+            debug!("exception: {:?}", e);
+        }
+        _ => panic!("unhandled trap"),
+    }
 }
 
 #[no_mangle]
@@ -35,5 +57,4 @@ pub fn user_trap_return() {
     extern "C" {
         fn _user_ret(ctx: *const TrapContext, satp: usize);
     }
-
 }
