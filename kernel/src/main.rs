22eb06b649010c81e5aa5786cbe25cb9a836742c
xxjay <760409558@qq.com>
2023-03-13 11:13:13 +0800

    feat: 初始化timer中断

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -7,11 +7,12 @@ use core::arch::global_asm;
 use core::arch::asm;
 use riscv::register::*;
 use proc::cpuid;
-
+use config::CPUS;
+use arch::riscv::register::*;
 extern crate alloc;
 
+mod arch;
 mod config;
-mod register;
 mod driver;
 mod proc;
 #[macro_use]
@@ -22,6 +23,7 @@ mod sync;
 mod syscall;
 
 global_asm!(include_str!("asm/entry.S"));
+global_asm!(include_str!("asm/kernelvec.S"));
 
 // 引导内核启动，设置M模式下的寄存器，之后跳转到内核入口进入S模式
 #[no_mangle]
@@ -43,12 +45,42 @@ pub fn rust_start() {
         pmpaddr0::write(config::PHYS_MEM_LIMIT - 1);
         // 物理地址保护，RWX=1, A=TOR, 范围[0,pmpaddr0)
         pmpcfg0::write(0b1111);
-
+        timer_init();
         // 将hartid保存到tp寄存器
         let cpuid = mhartid::read();
         asm!("mv tp, {}", in(reg) cpuid);
         asm!("mret");
     }
+}
+
+static mut TIMER_SCRATCH:[[usize; 5]; CPUS] = [[0usize; 5]; CPUS];
+
+// 初始化m时钟中断
+unsafe fn timer_init() {
+    // 每个cpu单独处理时间中断
+    let id = mhartid::read();
+    // 向clint提交中断间隔
+    let interval = 1000000;
+    add_mtimecmp(id, interval);
+
+    // scratch[0..=2] 用于保存寄存器
+    // scratch[3] : CLINT MTIMECMP地址
+    // scratch[4] : 时钟中断间隔
+    TIMER_SCRATCH[id][3] = mtie_cmp_addr(id);
+    TIMER_SCRATCH[id][4] = interval;
+
+    // mscratch寄存器记录scratch数组地址
+    mscratch::write(TIMER_SCRATCH[id].as_ptr() as usize);
+
+    // 设置机器中断处理器为timer_vec
+    extern "C" {
+        fn timervec();
+    }
+    mtvec::write(timervec as usize, mtvec::TrapMode::Direct);
+    // 开启machine中断
+    mstatus::set_mie();
+    // 开启machine模式时钟中断
+    mie::set_mtimer();
 }
 
 use core::sync::atomic::AtomicU8;
