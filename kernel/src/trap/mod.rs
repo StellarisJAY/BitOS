e309a28c449fe72edecfdfe6d36d2497d28be05a
xxjay <760409558@qq.com>
2023-03-14 15:54:05 +0800

    feat: 系统调用的陷入处理

--- kernel/src/trap/mod.rs
+++ kernel/src/trap/mod.rs
@@ -3,6 +3,10 @@ use riscv::register::{stvec, sepc};
 use riscv::register::scause::Interrupt::*;
 use riscv::register::scause::Exception::*;
 use context::TrapContext;
+use crate::proc::scheduler::{current_proc_trap_context, current_proc_satp, current_proc_trap_addr};
+use crate::syscall::handle_syscall;
+use crate::config::TRAMPOLINE;
+use core::arch::asm;
 
 pub mod context;
 
@@ -19,12 +23,18 @@ pub fn trap_init() {
 }
 
 #[no_mangle]
-pub fn user_trap_handler() {
+pub unsafe fn user_trap_handler() {
+    let mut ctx = current_proc_trap_context();
     let scause = scause::read();
     match scause.cause() {
         // 用户进程ecall导致的系统调用
         Exception(UserEnvCall) => {
-
+            // epc + 4使trap结束后能够跳到trap之后的一条指令
+            ctx.sepc += 4;
+            let ret = handle_syscall(ctx.a[7], [ctx.a[0], ctx.a[1], ctx.a[2]]);
+            // 因为syscall可能切换了另一个进程，所以这里要重新获取ctx
+            ctx = current_proc_trap_context();
+            ctx.a[0] = ret as usize;
         },
         // 由machine模式时间中断处理器抛出的S模式软件中断
         Interrupt(SupervisorSoft) => {
@@ -32,6 +42,7 @@ pub fn user_trap_handler() {
         },
         _ => panic!("unhandled trap"),
     }
+    user_trap_return();
 }
 
 #[no_mangle]
@@ -56,5 +67,12 @@ pub fn kernel_trap_handler() {
 pub fn user_trap_return() {
     extern "C" {
         fn _user_ret(ctx: *const TrapContext, satp: usize);
+        fn _user_vec();
+    }
+    let user_ret_va = _user_ret as usize - _user_vec as usize + TRAMPOLINE;
+    let satp = current_proc_satp();
+    let trap_context = current_proc_trap_addr() as *const TrapContext;
+    unsafe {
+        _user_ret(trap_context, satp);
     }
 }
