0d53994f6bef83a0d10f1010be1c8902ff6420ea
xxjay <760409558@qq.com>
2023-04-25 17:53:05 +0800

    fix: 用户进程地址空间权限错误

--- kernel/src/mem/app.rs
+++ kernel/src/mem/app.rs
@@ -3,7 +3,8 @@ use super::memory_set::*;
 use elf::ElfBytes;
 use elf::endian::AnyEndian;
 use crate::config::*;
-
+use super::page_table::*;
+use alloc::vec;
 const PT_LOAD: u32 = 1;
 
 // 应用程序虚拟地址空间布局：
@@ -25,32 +26,71 @@ impl MemorySet {
                 // vpn range
                 let start_va = VirtAddr(seg.p_vaddr as usize);
                 let end_va = VirtAddr(seg.p_vaddr as usize + seg.p_filesz as usize + PAGE_SIZE);
+                let flags = elf_flags_to_pte_flags(seg.p_flags as usize);
                 memset.insert_area(MemoryArea::new(
-                        start_va.vpn(),
+                    start_va.vpn(),
                         end_va.vpn(),
                         MapMode::Indirect,
-                        (seg.p_flags & 0b111) as usize), // RWX flags
+                        elf_flags_to_pte_flags(seg.p_flags as usize) | MemPermission::U.bits()), // RWX flags
                 Some(elf.segment_data(&seg).unwrap())); // copy data
                 max_vpn = end_va.vpn().0;
             }
         }
-        // 映射用户空间栈，起始地址与elf结束页中间间隔一个空页（可以通过缺页中断捕捉内存访问越界）
-        let stack_bottom_vpn = VirtPageNumber(max_vpn + 2);
-        let stack_top_vpn = VirtAddr(stack_bottom_vpn.base_addr() + USER_STACK_SIZE - 1).vpn();
+        // 在高地址映射用户栈
+        let (stack_bottom, stack_top) = user_stack_position();
         memset.insert_area(MemoryArea::new(
-                stack_bottom_vpn,
-                stack_top_vpn,
+                VirtAddr(stack_bottom).vpn(),
+                VirtAddr(stack_top).vpn(),
                 MapMode::Indirect,
-                MemPermission::R.bits() | MemPermission::W.bits()
+                MemPermission::R.bits() | MemPermission::W.bits() | MemPermission::U.bits() // 用户栈设置U mode，只允许用户模式访问
         ), None);
         // 映射Trampoline
         memset.map_trampoline();
         // 映射TrapContext
         memset.insert_area(MemoryArea::new(
-                VirtAddr(TRAP_CONTEXT).vpn(),
+            VirtAddr(TRAP_CONTEXT).vpn(),
                 VirtAddr(TRAP_CONTEXT + PAGE_SIZE).vpn(),
                 MapMode::Indirect,
-                MemPermission::R.bits() | MemPermission::W.bits()), None);
-        return (memset, elf.ehdr.e_entry as usize, stack_top_vpn.base_addr());
+                MemPermission::R.bits() | MemPermission::W.bits()), None); // trap_ctx 只在Supervisor访问，不设置U mode
+        return (memset, elf.ehdr.e_entry as usize, stack_top);
+    }
+}
+
+// elf flags 转换 pte flags
+fn elf_flags_to_pte_flags(p_flags: usize) -> usize {
+    // elf中的段全部是User mode访问
+    let mut flags: usize = MemPermission::U.bits();
+    if p_flags & 1 != 0 {
+        flags |= MemPermission::X.bits();
+    }
+    if p_flags & 2 != 0 {
+        flags |= MemPermission::W.bits();
+    }
+    if p_flags & 4 != 0 {
+        flags |= MemPermission::R.bits();
+    }
+    return flags;
+}
+
+#[allow(unused)]
+pub fn app_map_test(satp: usize) {
+    debug!("testing app map");
+    let page_table = PageTable::from_satp(satp);
+    let (stack_bottom, _) = user_stack_position();
+    // 测试用例：.text, entry_point, trap_ctx, user_stack
+    let cases = vec![0x10200, 0x10208, TRAP_CONTEXT, stack_bottom + PAGE_SIZE];
+    let expect_exec = vec![true, true, false, false];
+    let expect_read = vec![true, true, true, true];
+    let expect_write = vec![false, false, true, true];
+    let expect_umode = vec![true, true, false, true];
+
+    for (i, case) in cases.iter().enumerate() {
+        let pte = page_table.translate(VirtAddr(*case).vpn()).unwrap();
+        assert!(pte.is_valid(), "pte should be valid");
+        assert!(pte.is_usermode() == expect_umode[i], "pte shoule be usermode");
+        assert!(pte.is_executable() == expect_exec[i], "executable missmatch");
+        assert!(pte.is_readalbe() == expect_read[i], "readable missmatch");
+        assert!(pte.is_writable() == expect_write[i], "writable missmatch");
+        debug!("test case {} passed", i);
     }
 }
