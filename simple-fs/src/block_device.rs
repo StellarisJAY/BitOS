5d64bb94da1df345b960a393e083bfbd06f3e4f5
xxjay <760409558@qq.com>
2023-05-04 10:06:16 +0800

    fix: BlockDevice接口增加Send和Sync

--- simple-fs/src/block_device.rs
+++ simple-fs/src/block_device.rs
@@ -1,5 +1,5 @@
 // 块设备接口，定义从块设备读写数据的方法
-pub trait BlockDevice: Send + Sync{
+pub trait BlockDevice: Send + Sync {
     fn read(&self, block_id: usize, data: &mut [u8]);
     fn write(&self, block_id: usize, data: &[u8]);
 }
