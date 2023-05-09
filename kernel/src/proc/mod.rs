d47c6573a9e1dc697990dcb69815ab8876afec63
xxjay <760409558@qq.com>
2023-05-09 16:56:28 +0800

    refactor: 修改加载的应用

--- kernel/src/proc/mod.rs
+++ kernel/src/proc/mod.rs
@@ -1,32 +1,11 @@
-use crate::arch::riscv::register;
-use crate::config::CPUS;
-use crate::sync::cell::SafeCell;
+use crate::task::scheduler;
 use alloc::sync::Arc;
 
-pub mod context;
 pub mod loader;
 pub mod pcb;
 pub mod pid;
-pub mod scheduler;
-
-pub fn cpuid() -> usize {
-    unsafe { register::read_tp() }
-}
 
 pub fn init_processors() {
-    // 初始化proc manager（目前仅加载测试程序）
-    let mut manager = scheduler::MANAGER.lock();
-    let proc1 = Arc::new(pcb::ProcessControlBlock::from_elf_data(
-        loader::load_kernel_app("hello_world"),
-    ));
-    manager.push(proc1);
-    let proc2 = Arc::new(pcb::ProcessControlBlock::from_elf_data(
-        loader::load_kernel_app("fork_test"),
-    ));
-    manager.push(proc2);
-//    let proc2 = Arc::new(pcb::ProcessControlBlock::from_elf_data(
-//        loader::load_kernel_app("shell"),
-//    ));
-//    manager.push(proc2);
-    drop(manager);
+    pcb::ProcessControlBlock::from_elf_data(loader::load_kernel_app("hello_world"));
+    pcb::ProcessControlBlock::from_elf_data(loader::load_kernel_app("thread_test"));
 }
