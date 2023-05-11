2038446f90932cfea29c8f97e27bd842c7fc8eea
xxjay <760409558@qq.com>
2023-05-11 12:00:59 +0800

    feat: 分配数据块和inode，创建root目录inode

--- simple-fs/src/simple_fs.rs
+++ simple-fs/src/simple_fs.rs
@@ -1,7 +1,7 @@
 use crate::bitmap::{Bitmap, ALLOC_PER_BMAP_BLOCK};
 use crate::block_cache::get_block_cache_entry;
 use crate::block_device::BlockDevice;
-use crate::inode::INODES_PER_BLOCK;
+use crate::inode::{DiskInode, InodeType, INODES_PER_BLOCK, INODE_SIZE};
 use crate::layout::BLOCK_SIZE;
 use crate::super_block::SuperBlock;
 use alloc::sync::Arc;
@@ -17,9 +17,9 @@ pub struct SimpleFileSystem {
 impl SimpleFileSystem {
     // 在块设备上创建一个文件系统
     pub fn new(block_dev: Arc<dyn BlockDevice>, total_blocks: u32, inodes: u32) -> Self {
-        let inode_blocks = inodes * INODES_PER_BLOCK;
-        let inode_bmap_blocks = inode_blocks / ALLOC_PER_BMAP_BLOCK;
-        // 总块数减去一个超级块和inode块 = data块 + data_bmap块
+        let inode_blocks = inodes / INODES_PER_BLOCK;
+        let inode_bmap_blocks = inodes / ALLOC_PER_BMAP_BLOCK; // 一个块里面有多个inodes
+                                                               // 总块数减去一个超级块和inode块 = data块 + data_bmap块
         let remaining = total_blocks - inode_blocks - inode_bmap_blocks - 1;
         // 剩下的block里面，分成多个{一个bitmap块+可分配的data块}组合
         let data_bmap_blocks = remaining / (ALLOC_PER_BMAP_BLOCK + 1);
@@ -46,8 +46,8 @@ impl SimpleFileSystem {
         let first_data_bmap_blk = first_inode_bmap_blk + inode_bmap_blocks;
         let first_inode_block = first_data_bmap_blk + data_bmap_blocks;
         let first_data_block = first_inode_block + inode_blocks;
-        // 创建bitmap
-        let inode_bmap = Bitmap::new(first_inode_block, first_inode_bmap_blk, inode_bmap_blocks);
+        // inode bitmap是分配inode而不是块，所以序号从0开始到最后一个inode
+        let inode_bmap = Bitmap::new(0, first_inode_bmap_blk, inode_bmap_blocks);
         let data_bmap = Bitmap::new(first_data_block, first_data_bmap_blk, data_bmap_blocks);
         return Self {
             block_dev: block_dev,
@@ -72,11 +72,7 @@ impl SimpleFileSystem {
         let first_inode_block = first_data_bmap_blk + super_blk.data_bmap_blocks;
         let first_data_block = first_inode_block + super_blk.inode_blocks;
         // 创建bitmap
-        let inode_bmap = Bitmap::new(
-            first_inode_block,
-            first_inode_bmap_blk,
-            super_blk.inode_bmap_blocks,
-        );
+        let inode_bmap = Bitmap::new(0, first_inode_bmap_blk, super_blk.inode_bmap_blocks);
         let data_bmap = Bitmap::new(
             first_data_block,
             first_data_bmap_blk,
@@ -89,5 +85,43 @@ impl SimpleFileSystem {
             inode_start: first_inode_block,
             data_start: first_data_block,
         };
+    }
+
+    // 根据inode序号，获取inode所在的块的全局id、块内序号、块内偏移
+    pub fn get_inode_position(&self, inode_seq: u32) -> (u32, u32, u32) {
+        let inode_block_id = inode_seq / INODES_PER_BLOCK + self.inode_start;
+        let inner_seq = inode_seq % INODES_PER_BLOCK;
+        return (inode_block_id, inner_seq, inner_seq * INODE_SIZE);
+    }
+
+    // alloc_inode分配一个inode，获取从0开始在inode范围内的序号
+    pub fn alloc_inode(&mut self) -> Option<u32> {
+        self.inode_bitmap.alloc(Arc::clone(&self.block_dev))
+    }
+
+    pub fn dealloc_inode(&mut self, inode_seq: u32) {
+        self.inode_bitmap
+            .dealloc(inode_seq, Arc::clone(&self.block_dev));
+    }
+
+    pub fn alloc_data_block(&mut self) -> Option<u32> {
+        self.data_bitmap.alloc(Arc::clone(&self.block_dev))
+    }
+
+    pub fn dealloc_data_block(&mut self, data_block_seq: u32) {
+        self.data_bitmap
+            .dealloc(data_block_seq, Arc::clone(&self.block_dev));
+    }
+
+    pub fn create_root_dir(&mut self) -> u32 {
+        let inode_seq = self.alloc_inode().unwrap();
+        let (blk_id, _, offset) = self.get_inode_position(inode_seq);
+        get_block_cache_entry(blk_id, Arc::clone(&self.block_dev))
+            .unwrap()
+            .lock()
+            .modify(offset, |inode: &mut DiskInode| {
+                *inode = DiskInode::new(InodeType::Directory);
+            });
+        return inode_seq;
     }
 }
