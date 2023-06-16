3b39803ca77fda93dc8e74c4e34e56aa25310b6d
xxjay <760409558@qq.com>
2023-06-16 16:45:47 +0800

    test: hello_world打印命令行参数

--- user_lib/src/bin/hello.rs
+++ user_lib/src/bin/hello.rs
@@ -4,7 +4,11 @@
 #[macro_use]
 extern crate user_lib;
 #[no_mangle]
-pub fn main() -> i32 {
+pub fn main(argc: usize, argv: &[&'static str]) -> i32 {
     println!("Hello world");
+    println!("argc: {}", argc);
+    for (i, arg) in argv.iter().enumerate() {
+        println!("arg[{}] = {}", i, *arg);
+    }
     return 0;
 }
