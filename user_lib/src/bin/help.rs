673684d80ad1cca9d2933fd4f74fea713f3e84d2
xxjay <760409558@qq.com>
2023-06-20 12:26:09 +0800

    feat: 更新help

--- user_lib/src/bin/help.rs
+++ user_lib/src/bin/help.rs
@@ -5,12 +5,20 @@
 extern crate user_lib;
 #[no_mangle]
 pub fn main() -> i32 {
-    println!("available commands: ");
-    println!("hello_world:                 run the Rust hello world");
-    println!("fork_test:                   run a fork and waitpid test");
-    println!("thread_test:                 run a multi-thread test");
-    println!("timeshard_test:              run a test to see Round-robin with TimeShards");
-    println!("shell:                       open a new shell");
-    println!("exit:                        close current shell");
+    println!("Shell builtin commands: ");
+    println!("type                         Check the type of a command");
+    println!("cd                           Change the shell working directory.");
+    println!("exit                         Exit current shell process");
+    print!("\n");
+    println!("Applications: ");
+    println!("hello_world                  Run the Rust hello world");
+    println!("cat                          Print file's content");
+    println!("stat                         Print a file's stats");
+    println!("ls                           List files in a directory");
+    println!("echo                         Print a message");
+    println!("fork_test                    Run a fork and waitpid test");
+    println!("thread_test                  Run a multi-thread test");
+    println!("timeshard_test               Run a test to see Round-robin with TimeShards");
+    println!("shell                        Open a new shell");
     return 0;
 }
